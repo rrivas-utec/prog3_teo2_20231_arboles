8441212378f6e07d660ff758c79340b80580600e
rrivas <rrivas@utec.edu.pe>
2023-06-30 18:45:12 -0500

    actualizar bst

--- binary_search_tree.h
+++ binary_search_tree.h
@@ -6,22 +6,36 @@
 #define PROG3_TEO2_20231_ARBOLES_BINARY_SEARCH_TREE_H
 
 #include <memory>
+#include <functional>
 using namespace std;
 
-template <typename KeyType, typename ValueType>
+template <
+        typename KeyType,
+        typename ValueType,
+        typename Compare=less<KeyType>
+        >
 class binary_search_tree {
+    // Seccion de tipos
+    struct node_t; // Declaracion adelantada o forward
+    using item_t = pair<KeyType, ValueType>;
+    using shared_ptr_t = shared_ptr<node_t>;
+    using weak_ptr_t = weak_ptr<node_t>;
+
     struct node_t {
-        pair<KeyType, ValueType> item{};
-        shared_ptr<node_t> left;
-        shared_ptr<node_t> right;
-        weak_ptr<node_t> parent;
-        node_t(pair<KeyType, ValueType> item): item(item) {}
+        item_t item{};
+        shared_ptr_t left;
+        shared_ptr_t right;
+        weak_ptr_t parent;
+        node_t(const item_t& item, shared_ptr_t parent)
+            : item(item), parent(parent) {}
     };
-    shared_ptr<node_t> root;
+
+    shared_ptr_t root;
 
 public:
     binary_search_tree() = default;
-    void insert(pair<KeyType, ValueType> item) {
+
+    void insert(item_t item) {
         // Verificar si tiene root
         if (!root) {
             root = make_shared<node_t>(item);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "binary_search_tree.h"
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    binary_search_tree<string, int> bst;
+    // Insercion de valores
+    bst.insert({"M", 10});
+    bst.insert({"F", 20});
+    bst.insert({"P", 30});
+    bst.insert({"A", 40});
+    bst.insert({"T", 50});
+    // Recorrido a lo largo
+    bst.bfs([](auto& item) {
+        cout << item.first << " " << item.second << endl;
+    });
+    cout << "-----\n";
+    // Recorrido a la profundidad (in order)
+    bst.in_order([](auto& item) {
+        cout << item.first << " " << item.second << endl;
+    })
     return 0;
 }
